851798e79bab3c132dbdce1346972a879731dae0
tenczar <nicholas.tenczar@gmail.com>
2018-06-06 15:35:22 -0700

    [function/timeout] Add support for function timeouts exec endpoint. (#12)
    * [function/timeout] Add support for function timeouts exec endpoint.

--- function-server/main.py
+++ function-server/main.py
@@ -5,6 +5,8 @@ import os
 import signal
 import sys
 import traceback
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import TimeoutError
 
 import falcon
 import waitress
@@ -12,11 +14,14 @@ import waitress
 INPUT_ERROR = 'InputError'
 FUNCTION_ERROR = 'FunctionError'
 SYSTEM_ERROR = 'SystemError'
+HEALTHY = True
 
 
 class Health:
     @staticmethod
     def on_get(req, res):
+        if not HEALTHY:
+            res.status = falcon.HTTP_500
         res.body = '{}'
 
 
@@ -47,10 +52,19 @@ def process_msg(msg, handle):
     try:
         sys.stderr = stderr
         sys.stdout = stdout
-        r = handle(msg['context'], msg['payload'])
+        with ThreadPoolExecutor(max_workers=1) as executor:
+            future = executor.submit(handle, msg['context'], msg['payload'])
+            timeout = msg['context'].get('timeout', None)
+            r = future.result(None if timeout == None or timeout == 0 else timeout / 1000)
     except (ValueError, TypeError) as e:
         stacktrace = traceback.format_exc().splitlines()
         err = {'type': INPUT_ERROR, 'message': str(e), 'stacktrace': stacktrace}
+    except TimeoutError as e:
+        stacktrace = traceback.format_exc().splitlines()
+        err = {'type': SYSTEM_ERROR, 'message': str(e), 'stacktrace': stacktrace}
+        if not future.cancel():
+            global HEALTHY
+            HEALTHY = False
     except Exception as e:
         stacktrace = traceback.format_exc().splitlines()
         err = {'type': FUNCTION_ERROR, 'message': str(e), 'stacktrace': stacktrace}
@@ -97,7 +111,6 @@ def process_req(req, f):
                           ensure_ascii=False)
 
     return process_msg(msg, f)
-
 
 def module_and_name(s):
     return s.rsplit('.', 1)
